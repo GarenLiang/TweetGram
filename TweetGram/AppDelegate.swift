64f64e20a7b9baaf161a192fb873d3890f68ce07
GarenLiang <ideabreeze@gmail.com>
2017-05-14 16:02:26 -0700

    tweet mac app

---
+++ TweetGram/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TweetGram
+//
+//  Created by GarenLiang on 2017/5/14.
+//  Copyright © 2017年 GarenLiang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TweetGram/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TweetGram
+//
+//  Created by GarenLiang on 2017/5/14.
+//  Copyright © 2017年 GarenLiang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
